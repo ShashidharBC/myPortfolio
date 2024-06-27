b527fe1fe90a712fa2318fc1ca608a4baec476e6
ShashidharBC98 <shashidharbc.1dt17is079@gmail.com>
2024-06-27 10:45:10 +0530

    Added the Toast Message

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from "react";
 import "./styles.css";
 import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
   const form = useRef();
@@ -12,47 +14,53 @@ const ContactForm = () => {
 
     emailjs.sendForm("service_zmfyhzv", "template_ebob4qa", form.current).then(
       () => {
-        alert("YOUR MESSAGE IS SENT SUCCESSFULLY! :)");
+        toast.success("Email sent successfully!");
+        console.log("Email is sent!!!");
+        form.current.reset();
       },
       (error) => {
-        alert("FAILED TO SEND YOUR MESSAGE...", error.text);
+        toast.error("Failed to send email..");
+        console.error("Email error:", error);
       }
     );
   };
 
   return (
-    <form
-      className="contact-form textStyle shadow-xl"
-      onSubmit={sendEmail}
-      ref={form}
-    >
-      <div>
-        <label className="text-left text-sm" htmlFor="user_name">
-          Full Name
-        </label>
-        <input type="text" id="user_name" name="user_name" required />
-      </div>
-      <div>
-        <label className="text-left text-sm" htmlFor="user_email">
-          Your Email
-        </label>
-        <input type="email" id="user_email" name="user_email" required />
-      </div>
-      <div>
-        <label className="text-left text-sm" htmlFor="message">
-          Your Message
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          className="min-h-[120px] border border-red-50"
-          required
-        />
-      </div>
-      <button className="text-lg font-normal" type="submit">
-        Submit form
-      </button>
-    </form>
+    <div>
+      <form
+        className="contact-form textStyle shadow-xl"
+        onSubmit={sendEmail}
+        ref={form}
+      >
+        <div>
+          <label className="text-left text-sm" htmlFor="user_name">
+            Full Name
+          </label>
+          <input type="text" id="user_name" name="user_name" required />
+        </div>
+        <div>
+          <label className="text-left text-sm" htmlFor="user_email">
+            Your Email
+          </label>
+          <input type="email" id="user_email" name="user_email" required />
+        </div>
+        <div>
+          <label className="text-left text-sm" htmlFor="message">
+            Your Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="min-h-[120px] border border-red-50"
+            required
+          />
+        </div>
+        <button className="text-lg font-normal" type="submit">
+          Submit form
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
   );
 };
 
